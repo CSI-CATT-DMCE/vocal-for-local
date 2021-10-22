10efa30cef69cd5e8448089a721ddbc563f5f78c
CVM0410 <mahajanchirag.04@gmail.com>
2021-10-22 17:48:53 +0530

    uploaded files

--- VFL/settings.py
+++ VFL/settings.py
@@ -124,21 +124,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CSRIPY_TEMPLATE_PACK = "bootstrap4"
 
 
-
 STATIC_URL = '/static/'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'vocalforlocal.hacktoberfest@gmail.com'
-EMAIL_HOST_PASSWORD = 'vfl_hacktoberfest'
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
